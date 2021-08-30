8e8821b5c990fb7aea0b439c486fe52680a5e566
williamliang <williamliang@beanfun.com>
2021-08-31 01:46:31 +0800

    create sumAll test

--- array/sum_test.go
+++ array/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -12,4 +15,15 @@ func TestSum(t *testing.T) {
 			t.Errorf("got '%d' want '%d' given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSunAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// 切片(slice)不能使用運算符去比較，這邊使用 reflect.DeepEqual 去比對
+	// 如果比對的是切片和字串也會通過編譯，要特別注意
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
