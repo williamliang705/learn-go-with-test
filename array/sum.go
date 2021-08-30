36a659b13a3273dff03dfc0868aa657b56ae1ed7
williamliang <williamliang@beanfun.com>
2021-08-31 02:00:21 +0800

    add SumAllTails test

--- array/sum.go
+++ array/sum.go
@@ -16,3 +16,17 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			// 從切片中索引 1 取到最後一個元素
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
