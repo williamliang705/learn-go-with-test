21ee941a9d95da65f2ac937f6677a68da855fc7a
williamliang <williamliang@beanfun.com>
2021-10-08 02:17:29 +0800

    refactor struct testing used TableDriverTest

--- struct/shape_test.go
+++ struct/shape_test.go
@@ -14,23 +14,18 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10, 10}, 100},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		checkArea(t, rectangle, got)
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		checkArea(t, circle, got)
-	})
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
 }
