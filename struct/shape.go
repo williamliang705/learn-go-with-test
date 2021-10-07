484a6d253ea3bc2b0b7098e6cf10756bb17eeb22
williamliang <williamliang@beanfun.com>
2021-10-08 01:43:08 +0800

    add struct example and testing

--- struct/shape.go
+++ struct/shape.go
@@ -1,1 +1,14 @@
 package _struct
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
