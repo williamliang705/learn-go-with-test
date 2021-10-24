d7bd966379834cc140b6dfef7168bc3077ade246
williamliang <williamliang@beanfun.com>
2021-10-24 22:57:44 +0800

    refactor test case

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -3,11 +3,11 @@ package _map
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{
+	dictionary := Dictionary{
 		"test": "this is just a test",
 	}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search(dictionary, "test")
 	want := "this is just a test"
 
 	assertStrings(t, got, want)
