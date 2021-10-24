72c8d3bc8dad9fce17d334dbb679288f9bf9000b
williamliang <williamliang@beanfun.com>
2021-10-24 17:19:10 +0800

    replace int to Bitcoin type

--- pointer/wallet.go
+++ pointer/wallet.go
@@ -1,13 +1,25 @@
 package pointer
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
