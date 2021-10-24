d96217e0653b331338cc36e9bd07bac3aec76a35
williamliang <williamliang@beanfun.com>
2021-10-24 22:54:10 +0800

    refactor test case

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -10,6 +10,11 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %s want %s given, %s", got, want, "test")
 	}
